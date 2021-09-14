ff82407a18668a9359d954251ba539d118248989
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-13 22:36:09 -0500

    pep8!

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,16 +6,16 @@ if __name__ == '__main__':
     from sys import argv
     import requests
 
-    if len(sys.argv) == 2:
+    if len(argv) == 2:
         url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
         res = requests.get(url)
         name = res.json()['username']
         todo = requests.get("https://jsonplaceholder.typicode.com/todos")
-        with open(sys.argv[1] + '.csv', "w") as csv_file:
+        with open(argv[1] + '.csv', "w") as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=",", quotechar='"',
                                     quoting=csv.QUOTE_MINIMAL)
 
             for item in todo.json():
-                if item['userId'] == int(sys.argv[1]):
-                    csv_writer.writerow([sys.argv[1], name, item['completed'],
+                if item['userId'] == int(argv[1]):
+                    csv_writer.writerow([argv[1], name, item['completed'],
                                          item['title']])
