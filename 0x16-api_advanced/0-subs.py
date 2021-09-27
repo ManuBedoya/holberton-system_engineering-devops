68e613633331215887b5651ac7c62cf64afe99c5
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-27 15:24:37 -0500

    [Update](All right)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """ get the  number of suscribers """
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Holberton'}
+    headers = {'User-Agent': 'Manu'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""Module for api reddit
+"""
 import requests
-import user_agent
-import sys
 
 
 def top_ten(subreddit):
-    if (len(sys.argv) == 2):
-        user_ag = {'User-Agent': user_agent.generate_user_agent()}
-        url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """display the 10 top
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Manu'}
 
-        response = requests.get(url, headers=user_ag)
-        data = response.json()
-        try:
-            for item in data['data']['children']:
-                print(item['data']['title'])
-        except KeyError:
-            print('None')
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+
+    if response.status_code == 200:
+        for item in data['data']['children']:
+            print(item['data']['title'])
+    else:
+        print 'None'
