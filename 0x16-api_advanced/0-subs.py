2217a2038d024911d14535352fc4cdaa8c713930
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-27 15:13:45 -0500

    [Update](user agent)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """Method to get suscribers
     """
     if (len(sys.argv) == 2):
-        user_ag = {'User-Agent': 'Hoberton'}
+        user_ag = {'User-Agent': 'HobertonSchool'}
         url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
         response = requests.get(url, headers=user_ag)
