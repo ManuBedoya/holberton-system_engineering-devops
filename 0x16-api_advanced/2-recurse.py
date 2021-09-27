e84cab7d1e97da255d4391d86b5f086e012b6fa3
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-27 17:03:28 -0500

    [Update](change all)

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,23 +4,29 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=""):
     """recursive method to get the all hot list
     """
-    user_ag = {'User-Agent': 'Manu'}
-    try:
-        if after is None:
-            url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        else:
-            url = 'https://www.reddit.com/r/{}/hot.json?after={}'
-            url = url.format(subreddit, after)
-    except KeyError:
+    headers = {'User-Agent': 'Manu'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    if after is None:
+        return hot_list
+
+    params = {'limit': 100, 'after': after}
+    response = requests.get(url, params=params, headers=headers,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
         return None
 
-    response = requests.get(url, headers=user_ag, allow_redirects=False)
-    data = response.json()['data']['children'][0]['data']['title']
-    hot_list.append(data)
-    if response.json()['data']['after'] is not None:
-        return recurse(subreddit, hot_list, response.json()['data']['after'])
-    else:
-        return hot_list
+    try:
+        catch = response.json()
+        data = catch.get('data')
+        after = catch.get('after')
+
+        for item in data.get('children'):
+            hot_list.append(item.get('data').get('title'))
+    except:
+        return None
+    return recurse(subreddit, hot_list, after)
