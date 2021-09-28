c92afe640c861dc51f88fc33e9edae93a84e1351
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-28 09:07:41 -0500

    [Add](All)

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix in settings of wordpress
+exec { 'fix wordpress':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
